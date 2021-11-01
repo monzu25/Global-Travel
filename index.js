54d12ae9d4cc7706238d663cf6aebe23aaad9cd7
MD. MONZU <monzu.ctg25@gmail.com>
2021-11-01 18:49:27 +0600

    Server Update

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const axios = require('axios').default;
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require("dotenv").config();
 const ObjectId = require('mongodb').ObjectId;
 
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 
 
 app.use(cors());
 app.use(express.json());
-
+// app.use(axios());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.11wfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -83,6 +86,8 @@ async function run() {
 
         });
 
+
+
         const orderCollection = database.collection('orderCollection');
         app.post('/newOrder', async (req, res) => {
             const newOrder = req.body;
@@ -124,13 +129,31 @@ async function run() {
 
 
 
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            console.log('deleting user with id ', result);
+            res.json(result);
+            console.log(result);
+        })
+
+
+
 
         // GET ordered Data
-        app.get('/cart', async (req, res) => {
-            const cursor = cartCollection.find({});
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
             const orders = await cursor.toArray();
             res.send(orders);
         })
+        app.get('/', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
 
 
     }
@@ -141,9 +164,9 @@ async function run() {
 
 run().catch(console.dir)
 
-app.get('/', (req, res) => {
-    res.send('This is my CRUD Server is runnig! Woow');
-});
+// app.get('/', (req, res) => {
+//     res.send('This is my CRUD Server is runnig! Woow');
+// });
 
 app.listen(port, () => {
     console.log('Running Server on port', port);
